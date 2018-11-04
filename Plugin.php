6c43005dc7d25f79a08ae2cdabade4760a2ad9ba
zyuzhi <4900169+zyuzhi@users.noreply.github.com>
2018-11-04 13:50:02 +0800

    update version info

--- Plugin.php
+++ Plugin.php
@@ -5,7 +5,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package MardownKatex
  * @author zyuzhi
- * @version 1.0.0
+ * @version 1.0.1
  * @link https://blog.zyuzhi.me
  */
 class MarkdownKatex_Plugin implements Typecho_Plugin_Interface
